2e10893e0207a8541e28aacecc798b11fffeea1f
mahajan022 <anuragg7051@gmail.com>
2026-03-29 14:09:48 +0530

    had

---
+++ Downloads/riya-cargo-updated (1)/riya-cargo/src/images.js
@@ -0,0 +1,18 @@
+export const LOGO        = "/images/logo.jpg";
+export const IMG_WORKERS = "/images/workers.jpg";
+export const IMG_FURN    = "/images/furniture.jpg";
+export const IMG_BOXES   = "/images/boxes.jpg";
+export const IMG_INDOOR  = "/images/indoor.jpg";
+export const IMG_TRUCK   = "/images/truck.jpg";
+export const IMG_WH1     = "/images/warehouse1.jpg";
+export const IMG_WH2     = "/images/warehouse2.jpg";
+export const IMG_OFFICE  = "/images/office.jpg";
+
+// Real site photos (added 29 Mar 2026)
+export const REAL_01 = "/images/real_01.jpg";
+export const REAL_02 = "/images/real_02.jpg";
+export const REAL_03 = "/images/real_03.jpg";
+export const REAL_04 = "/images/real_04.jpg";
+export const REAL_05 = "/images/real_05.jpg";
+export const REAL_06 = "/images/real_06.jpg";
+export const REAL_07 = "/images/real_07.jpg";
